dd0fe52bbc1aba8ec89d2933080291ed18d7ad99
goziecodes <ezechukwuchigozie@gmail.com>
2021-01-18 21:06:22 +0100

    first commit, setup the react app, and used fetch to access the enye API

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
 
 function App() {
+  const [cardDetails, setCardDetails ] = useState([]);
+
+  const fetchData = async () => {
+    const response = await fetch('https://api.enye.tech/v1/challenge/records');
+    const data = await response.json();
+    return data
+
+
+  }
+
+
+
+  useEffect(()=>{
+  fetchData()
+   .then(res =>{
+     setCardDetails(res.records.profiles);
+     console.log(res, 'im here mofo')
+   })
+  // console.log(okay, "yes na")
+
+
+
+  },[])
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>I am enye</h1>
+      <ul>
+        {
+       cardDetails.length <= 0 ?  'Loading.....' : cardDetails.map((card,i) => (
+            <li key={i}>{card.FirstName}</li>
+       )) 
+        }
+      </ul>
+      {console.log(cardDetails.length,'cardd')}
     </div>
   );
 }
